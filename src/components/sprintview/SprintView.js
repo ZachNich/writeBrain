1fd7740c2c798a14631bf52a1c1bdd5b636aecb4
Zach Nicholson <raiste.doctow@gmail.com>
2020-09-21 23:04:41 -0500

    working sprintview

--- src/components/sprintview/SprintView.js
+++ src/components/sprintview/SprintView.js
@@ -6,11 +6,11 @@ const SprintView = props => {
     return (
         <div className="sprint_view">
             <div className="sprint_stats">
-                <Card>{props.sprint.mood_before.name}</Card>
-                <Card>{props.sprint.mood_after.name}</Card>
-                <Card>{props.sprint.body.split(" ").length}</Card>
-                <Card>{(props.sprint.ended_at - props.sprint.started_at) / props.sprint.body.split(" ").length}</Card>
-                <Card>{props.sprint.ended_at - props.sprint.started_at}</Card>
+                <div className="sprint_stat_icon">{props.sprint.mood_before.name}</div>
+                <div className="sprint_stat_icon">{props.sprint.mood_after.name}</div>
+                <div className="sprint_stat_icon">{props.sprint.body.split(" ").length}</div>
+                <div className="sprint_stat_icon">{(props.sprint.ended_at - props.sprint.started_at) / props.sprint.body.split(" ").length}</div>
+                <div className="sprint_stat_icon">{props.sprint.ended_at - props.sprint.started_at}</div>
             </div>
             <Card body>
                 {props.sprint.body}
