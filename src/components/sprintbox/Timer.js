0ed8670600e4e916ab151346ebdbeae9f17130a4
Zach Nicholson <raiste.doctow@gmail.com>
2020-09-23 14:22:04 -0500

    added sound for end of timer

--- src/components/sprintbox/Timer.js
+++ src/components/sprintbox/Timer.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import useInterval from '../../hooks/useInterval'
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
 
 const Timer = (props, {hours = 0, minutes = 0, seconds = 0}) => {
     const [over, setOver] = useState(false)
     const [timeString, setTimeString] = useState("")
 
+    const startSound = () => {
+        const alarmSound = new Audio('/media/alarm_sound.wav')
+        alarmSound.play()
+    }
+
     const countdown = e => {
         if (!over) {
             if (props.timeLeft.hours === 0 && props.timeLeft.minutes === 0 && props.timeLeft.seconds === 0) {
                 setOver(true)
+                startSound()
             } else if (props.timeLeft.seconds === 0 && props.timeLeft.minutes === 0) {
                 props.setTimeLeft({
                     hours: props.timeLeft.hours - 1,
@@ -63,11 +67,9 @@ const Timer = (props, {hours = 0, minutes = 0, seconds = 0}) => {
     }, [props.timeLeft])
 
     return (
-        <>
-            <div>
-                {timeString}
-            </div>
-        </>
+        <div className="timer">
+            {timeString}
+        </div>
     )
 }
 
